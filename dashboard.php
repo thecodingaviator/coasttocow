0893159184dc0eb7627cfb8ee9ea7377fca63018
Parth Parth <34807532+thecodingaviator@users.noreply.github.com>
2023-06-02 16:08:33 -0400

    dashboard

--- dashboard.php
+++ dashboard.php
@@ -1,26 +1,3 @@
-<?php
-session_start();
-
-// Check if the user is not logged in, redirect to login page
-if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
-    header("Location: index.php");
-    exit();
-}
-
-// Logout functionality
-if (isset($_POST['logout'])) {
-    // Clear session variables
-    $_SESSION = array();
-
-    // Destroy the session
-    session_destroy();
-
-    // Redirect to the login page
-    header("Location: index.php");
-    exit();
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -29,15 +6,15 @@ if (isset($_POST['logout'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
+
+    <link rel="stylesheet" href="css/dashboard-common.css">
 </head>
 
 <body>
-    <h1>Welcome to the Dashboard</h1>
-    <!-- Your dashboard content here -->
-
-    <form action="" method="POST" name="logoutForm">
-        <button type="submit" name="logout">Logout</button>
-    </form>
+  <?php include "navbar.php"; ?>
+    <div class="content">
+        <h1>Dashboard</h1>
+    </div>
 </body>
 
 </html>
---
+++ navbar.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+
+// Check if the user is not logged in, redirect to login page
+if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
+    header("Location: index.php");
+    exit();
+}
+
+// Logout functionality
+if (isset($_POST['logout'])) {
+    // Clear session variables
+    $_SESSION = array();
+
+    // Destroy the session
+    session_destroy();
+
+    // Redirect to the login page
+    header("Location: index.php");
+    exit();
+}
+?>
+
+<link rel="stylesheet" href="css/navbar.css">
+
+<nav>
+    <div>
+        <h1>Coast to Cow</h1>
+    </div>
+    <ul>
+        <li><a href="page1.php">Page 1</a></li>
+        <li><a href="page2.php">Page 2</a></li>
+        <li><a href="page3.php">Page 3</a></li>
+    </ul>
+    <form action="" method="POST" name="logoutForm">
+        <button type="submit" name="logout">Logout</button>
+    </form>
+</nav>
