ce6f13f3780853387a7bd48cecb3b15a275cef87
Parth Parth <34807532+thecodingaviator@users.noreply.github.com>
2023-06-25 17:12:17 -0400

    files working

--- utils/config.php
+++ utils/config.php
@@ -3,7 +3,7 @@
 include 'credentials.php';
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=$dbname;port=$port", $username, $password);
+  $conn = new PDO("mysql:host=$server;dbname=$dbname;port=$port", $username, $password);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
