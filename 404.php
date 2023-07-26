c438dcdf5525f2669e60ce44f0b45ec1a9c0b8dd
Parth Parth <34807532+thecodingaviator@users.noreply.github.com>
2023-07-26 19:46:25 +0530

    404 page

---
+++ 404.php
@@ -0,0 +1,55 @@
+<?php
+// File: index.php
+// Author: Parth Parth
+// Purpose: 404 page
+// Last modified: 07/26/2023
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>404 | Coast To Cow Consumer</title>
+  <link rel="stylesheet" href="utils/css/normalize.css">
+  <link rel="stylesheet" href="utils/css/universal.css">
+</head>
+
+<body>
+  <div class="div-404">
+    <h2>Oops! You seem to have reached somewhere unexpected. If you expect something here, contact
+      kara.kugelmeyer@colby.edu.
+      Click <a href="/index.php">here</a> to go to home.
+    </h2>
+  </div>
+
+
+</body>
+
+<style>
+  body {
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .div-404 {
+    width: 60%;
+    text-align: justify;
+  }
+
+  a {
+    color: #0000ff !important;
+    text-decoration: underline !important;
+  }
+
+  video {
+    height: 50%;
+    margin: 0 !important;
+  }
+</style>
+
+</html>
