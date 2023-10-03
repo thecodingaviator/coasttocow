dbc9b60494798cdcca0fa1cec3a1f253457d08e1
Gordon Doore <gcdoor25@colby.edu>
2023-10-03 10:41:52 -0400

    just changing the chart so pushing directly

--- explore_dash.php
+++ explore_dash.php
@@ -88,25 +88,14 @@ $count = array_column($data, 'count');
                 <h2>Database Entry Breakdown</h2>
                 <p>Number of entries in each table</p>
               </header>
-              <canvas id="myChart"></canvas>
+              <canvas id="myBarChart" width="400" height="300"></canvas>
             </div>
             <!-- <div class="chart-container">
               <header>
-                <h2>Chart 2</h2>
+                <h2>Second Chart Title</h2>
+                <p>Second Chart Description</p>
               </header>
-              <canvas id="chart2"></canvas>
-            </div>
-            <div class="chart-container">
-              <header>
-                <h2>Chart 3</h2>
-              </header>
-              <canvas id="chart3"></canvas>
-            </div>
-            <div class="chart-container">
-              <header>
-                <h2>Chart 4</h2>
-              </header>
-              <canvas id="chart4"></canvas>
+              <canvas id="mySecondChart" width="400" height="300"></canvas>
             </div> -->
           </div>
         </div>
@@ -115,14 +104,15 @@ $count = array_column($data, 'count');
   </div>
 
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-<script>
+  <script>
     // Create a new chart using Chart.js library
-    var ctx = document.getElementById('myChart').getContext('2d');
+    var ctx = document.getElementById('myBarChart').getContext('2d');
     var chart = new Chart(ctx, {
-      type: 'pie',
+      type: 'bar', // Change chart type to 'bar'
       data: {
         labels: <?php echo json_encode($labels); ?>,
         datasets: [{
+          label: 'Number of entries',
           data: <?php echo json_encode($count); ?>,
           backgroundColor: [
             'rgba(255, 99, 132, 0.2)',
@@ -148,40 +138,75 @@ $count = array_column($data, 'count');
         }]
       },
       options: {
-        legend: {
-          labels: {
-            generateLabels: function (chart) {
-              var data = chart.data;
-              if (data.labels.length && data.datasets.length) {
-                return data.labels.map(function (label, i) {
-                  var meta = chart.getDatasetMeta(0);
-                  var ds = data.datasets[0];
-                  var arc = meta.data[i];
-                  var customLabel = label + ' (' + ds.data[i] + ')';
-                  var arcOpts = chart.options.elements.arc;
-                  var fill = Chart.helpers.getValueAtIndexOrDefault;
-
-                  var arcBackgroundColor = fill(arc.custom && arc.custom.backgroundColor, arcOpts.backgroundColor, i, data.length);
-                  var arcBorderColor = fill(arc.custom && arc.custom.borderColor, arcOpts.borderColor, i, data.length);
-                  var borderWidth = fill(arc.custom && arc.custom.borderWidth, arcOpts.borderWidth, i, data.length);
-
-                  return {
-                    text: customLabel,
-                    fillStyle: arcBackgroundColor,
-                    strokeStyle: arcBorderColor,
-                    lineWidth: borderWidth,
-                    hidden: isNaN(ds.data[i]) || meta.data[i].hidden,
-                    index: i
-                  };
-                });
-              }
-              return [];
+        scales: {
+          y: {
+            beginAtZero: true,
+            title: {
+              display: true,
+              text: 'Number of entries'
             }
+          }
+        },
+        plugins: {
+          legend: {
+            display: false
           }
         }
       }
     });
-  </script>
+
+    // Create a new chart for the second chart
+    var ctx2 = document.getElementById('mySecondChart').getContext('2d');
+    var chart2 = new Chart(ctx2, {
+      type: 'bar', // Change chart type to 'bar' for the second chart
+      data: {
+        labels: <?php echo json_encode($labels); ?>,
+        datasets: [{
+          label: 'Number of entries',
+          data: <?php echo json_encode($count); ?>, // Replace with data for the second chart
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(233, 30, 99, 0.2)',
+            'rgba(33, 150, 243, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(233, 30, 99, 1)',
+            'rgba(33, 150, 243, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+            title: {
+              display: true,
+              text: 'Number of entries'
+            }
+          }
+        },
+        plugins: {
+          legend: {
+            display: false
+          }
+        }
+      }
+    });
+
+</script>
+
 </body>
 
 </html>
