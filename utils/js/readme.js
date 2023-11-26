4ae5938b80430ab1545b5f564dd2748de3266b95
Parth Parth <34807532+thecodingaviator@users.noreply.github.com>
2023-11-25 21:44:29 -0500

    readme

--- utils/js/readme.js
+++ utils/js/readme.js
@@ -1,37 +1,3 @@
-document.getElementById('add-author-btn').addEventListener('click', function () {
-  var container = document.getElementById('primary_contact_container');
-  var authors = container.querySelectorAll('.author-input').length;
-  var newAuthor = authors + 1;
-
-  var inputHTML = '<div class="author-input" id="author-input-' + newAuthor + '">' +
-    '<label for="primary_contact' + newAuthor + '">Author ' + newAuthor + ':</label>' +
-    '<input type="text" id="primary_contact' + newAuthor + '" class="primary_contact" name="authors[]">' +
-    '<button type="button" class="remove-author-btn">Remove</button>' +
-    '</div>';
-
-  container.insertAdjacentHTML('beforeend', inputHTML);
-  attachRemoveAuthorEvent();
-});
-
-function attachRemoveAuthorEvent() {
-  var removeButtons = document.querySelectorAll('.remove-author-btn');
-  removeButtons.forEach(function (button) {
-    button.removeEventListener('click', removeAuthor);
-    button.addEventListener('click', removeAuthor);
-  });
-}
-
-function attachFillFormEvent() {
-  var fillFormButton = document.getElementById('fill_from_title');
-  fillFormButton.removeEventListener('click', fillForm);
-  fillFormButton.addEventListener('click', fillForm);
-}
-
-function removeAuthor(event) {
-  var authorDiv = event.target.parentNode;
-  authorDiv.parentNode.removeChild(authorDiv);
-}
-
 function fillForm() {
 
   var selectedTitle = document.getElementById('selected_title').value;
